24a5f5a8129126d4e88e692b6fd46c5657d94c99
Your Name <you@example.com>
2025-06-03 12:05:23 -0500

    Subo ejercicios base de datos

---
+++ db/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+#variable cadena de conexion:
+MARIADB_URL= 'mysql+pymysql://root:admin@localhost:3315/pyshop'
+
+#crear el objeto de conexion la base de datos
+engine= create_engine(MARIADB_URL) 
+
+#plantilla base para los modelos 
+Base = declarative_base()
